133368e3e66864135f634ab72c93f4f82fd7bb86
Rafael Bedia <rafael.bedia@gmail.com>
2025-03-29 20:46:05 -0400

    Experiment with using pip-audit as an alternative to safety.
    pip-audit is quite slow compared to safety, seemingly due to resolving
    all dependencies again even though poetry has already taken care of it.

--- noxfile.py
+++ noxfile.py
@@ -154,11 +154,20 @@ def safety(session: Session) -> None:
     session.run("safety", "check", "--full-report", f"--file={requirements}")
 
 
+@session(python=python_versions[0])
+def audit(session: Session) -> None:
+    """Scan dependencies for insecure packages."""
+    requirements = session.poetry.export_requirements()
+    session.install("pip-audit")
+    session.run("pip-audit", "-r", requirements)
+
+
 @session(python=python_versions)
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
     args = session.posargs or ["src", "tests", "docs/conf.py"]
     session.install(".")
+    install_poetry_groups(session, "docs")
     session.install("mypy", "pytest")
     session.run("mypy", *args)
     if not session.posargs:
